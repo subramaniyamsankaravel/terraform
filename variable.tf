0065fab316673764039e1b8aad9ba56562d7acd3
Subramaniyam Sankaravel <37463732+subramaniyamsankaravel@users.noreply.github.com>
2023-05-11 21:37:18 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "location" {
+  type    = string
+}
+
+variable "resource_group_name" {
+  type    = string
+}
+
+variable "Operating_system" {
+  type    = string
+}
+variable "network_security_group" {
+  type    = string
+}
+
+variable "virtual_network" {
+  type    = string
+}
+variable "subnet1" {
+  type    = string
+}
+
+variable "virtual_machine1" {
+  type    = string
+}
+
+variable "virtual_machine2" {
+  type    = string
+}
